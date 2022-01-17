728e95addb9f66f10e06d592adca75e2364f0d08
mrofftin <mrofftin@gmail.com>
2022-01-17 18:59:04 +0900

    init

---
+++ src/test/java/co/mr/demo_01_17/Demo0117ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mr.demo_01_17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0117ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
